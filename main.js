7bfafa91d93f6bc253b94ae52123a054d41669ae
elnur29 <mamasadykova06@gmail.com>
2023-12-27 16:42:21 +0600

    initial

---
+++ main.js
@@ -0,0 +1,17 @@
+
+
+const burgerFunc = (btnClass, navClass, showClass) =>{
+    let btn = document.getElementById(btnClass);
+
+    let nav = document.getElementById(navClass);
+
+    btn.onclick = () =>{
+        if(nav.className.includes(showClass)){
+            nav.className = navClass
+        } else {
+            nav.className += ' ' + showClass
+        }
+    }
+}
+
+burgerFunc('burger-btn', 'header-nav', 'header-nav-show')
